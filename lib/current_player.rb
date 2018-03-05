68518ea19e97ea04d784a22b768a8facb75d9968
Jerry P <jpull@jpull.com>
2018-03-05 23:50:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,5 @@
 def turn_count(board)
-  turns = 0
-  board.each do |position|
-    if position == "X" || position == "O"
-      turns += 1
-    end
-  end
-  turns
+board.count{|token| token == "X" || token == "O"}
 end
 
 def current_player(board)
